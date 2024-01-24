4c818f31d97eb584eceee7d9f765ed7740599d87
jarvandev <jarvan.z@outlook.com>
2024-01-24 17:42:09 +0800

    fix lint error

--- packages/apps-sdk/src/checkCall.ts
+++ packages/apps-sdk/src/checkCall.ts
@@ -1,9 +1,8 @@
 // Copyright 2023-2024 dev.mimir authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import '@polkadot/api-augment/substrate';
-
 import type { ApiPromise } from '@polkadot/api';
+import type {} from '@polkadot/api-augment/substrate';
 import type { Call } from '@polkadot/types/interfaces';
 import type { IMethod } from '@polkadot/types/types';
 
