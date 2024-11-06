f1f79e759c0e40aa281ba3ba1f1ddabdd2328fbc
jarvandev <jarvan.z@outlook.com>
2024-11-06 11:26:06 +0800

    Add checkCallAsync function for up to date integration with mimir

---
+++ .changeset/orange-shirts-hang.md
@@ -0,0 +1,6 @@
+---
+"@mimirdev/apps-sdk": major
+"@mimirdev/apps-inject": major
+---
+
+Add `checkCallAsync` func for up to date integration with mimir
--- packages/apps-sdk/src/checkCall.ts
+++ packages/apps-sdk/src/checkCall.ts
@@ -3,12 +3,16 @@
 
 import type { ApiPromise } from '@polkadot/api';
 import type {} from '@polkadot/api-augment/substrate';
-import type { Call } from '@polkadot/types/interfaces';
+import type { AccountId, Address, Call } from '@polkadot/types/interfaces';
 import type { IMethod } from '@polkadot/types/types';
+
+import { addressEq, encodeMultiAddress } from '@polkadot/util-crypto';
+
+import { sendMessage } from '@mimirdev/apps-sdk/sendMessage';
 
 function findFinalCall(api: ApiPromise, call: Call | IMethod): Call | IMethod {
   if (api.tx.multisig.asMulti.is(call)) {
-    return findFinalCall(api, call.args[3]);
+    return findFinalCall(api, api.registry.createType('Call', call.args[3]));
   } else if (api.tx.proxy?.proxy?.is(call)) {
     return findFinalCall(api, call.args[2]);
   } else {
@@ -16,6 +20,9 @@ function findFinalCall(api: ApiPromise, call: Call | IMethod): Call | IMethod {
   }
 }
 
+/**
+ * @deprecated Use checkCallAsync for up to date integration with mimir
+ */
 export function checkCall(api: ApiPromise, multisigCall: Call | IMethod, expectCall: Call | IMethod): boolean {
   const finalCall = findFinalCall(api, multisigCall);
 
@@ -28,5 +35,59 @@ export function checkCall(api: ApiPromise, multisigCall: Call | IMethod, expectC
     return false;
   } else {
     return true;
+  }
+}
+
+export async function checkCallAsync(
+  api: ApiPromise,
+  call: Call | IMethod,
+  address: string | Uint8Array | AccountId | Address,
+  expectCall: Call | IMethod,
+  expectAccount: string | Uint8Array | AccountId | Address
+): Promise<boolean> {
+  if (call.toHex() === expectCall.toHex() && addressEq(address.toString(), expectAccount.toString())) {
+    return true;
+  }
+
+  if (api.tx.multisig.asMulti.is(call)) {
+    const nextCall = api.registry.createType('Call', call.args[3]);
+    const nextAddress = encodeMultiAddress([address.toString(), ...call.args[1]], call.args[0]);
+
+    return checkCallAsync(api, nextCall, nextAddress, expectCall, expectAccount);
+  } else if (api.tx.multisig.approveAsMulti.is(call)) {
+    const hash = call.args[3];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const data = await sendMessage('pub(call.get)', hash.toHex());
+
+    const nextCall = api.createType('Call', data);
+
+    if (nextCall.hash.toHex() !== hash.toHex()) {
+      throw new Error('approveAsMulti call hash not match original call hash');
+    }
+
+    const nextAddress = encodeMultiAddress([address.toString(), ...call.args[1]], call.args[0]);
+
+    return checkCallAsync(api, nextCall, nextAddress, expectCall, expectAccount);
+  } else if (api.tx.proxy?.proxy?.is(call)) {
+    const nextCall = api.registry.createType('Call', call.args[2]);
+    const nextAddress = call.args[0].toString();
+
+    return checkCallAsync(api, nextCall, nextAddress, expectCall, expectAccount);
+  } else if (api.tx.proxy?.announce?.is(call)) {
+    const hash = call.args[1];
+    const nextAddress = call.args[0].toString();
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const data = await sendMessage('pub(call.get)', hash.toHex());
+
+    const nextCall = api.createType('Call', data);
+
+    if (nextCall.hash.toHex() !== hash.toHex()) {
+      throw new Error('announce call hash not match original call hash');
+    }
+
+    return checkCallAsync(api, nextCall, nextAddress, expectCall, expectAccount);
+  } else {
+    return false;
   }
 }
